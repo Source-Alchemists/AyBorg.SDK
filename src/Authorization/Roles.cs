c99a428e8fa7df267f334b6a5cc2465d17fd1e7a
Light4Code <light4code@gmail.com>
2022-11-01 21:37:24 +0100

    Roles

---
+++ src/Authorization/Roles.cs
@@ -0,0 +1,8 @@
+namespace Atomy.SDK.Authorization;
+
+public static class Roles
+{
+    public const string Administrator = "Administrator";
+    public const string Engineer = "Engineer";
+    public const string Auditor = "Auditor";
+}
