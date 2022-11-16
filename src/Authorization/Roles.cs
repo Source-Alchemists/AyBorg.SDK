e353e90c03da94d59c5da8aa087204814d2918f7
Light4Code <light4code@gmail.com>
2022-11-16 18:47:52 +0100

    Added reviewer roll

--- src/Authorization/Roles.cs
+++ src/Authorization/Roles.cs
@@ -5,4 +5,5 @@ public static class Roles
     public const string Administrator = "Administrator";
     public const string Engineer = "Engineer";
     public const string Auditor = "Auditor";
+    public const string Reviewer = "Reviewer";
 }
