07b97f4ac9e555805d06bf689591e6b7deacdca3
Light4Code <light4code@gmail.com>
2023-01-23 14:34:46 +0100

    Result EventLogType

--- src/Common/Logging/EventLogType.cs
+++ src/Common/Logging/EventLogType.cs
@@ -16,9 +16,18 @@ public enum EventLogType
     Connect = 42000,
     [Description("Disconnect")]
     Disconnect = 42001,
+    // Common
+    [Description("Result")]
+    Result = 42010,
     // Engine
     [Description("Engine State")]
     EngineState = 42100,
     [Description("Plugin State")]
-    PluginState = 42110
+    PluginState = 42110,
+    [Description("Project State")]
+    ProjectState = 42200,
+    [Description("Project saved")]
+    ProjectSaved = 42201,
+    [Description("Project removed")]
+    ProjectRemoved = 42202
 }
