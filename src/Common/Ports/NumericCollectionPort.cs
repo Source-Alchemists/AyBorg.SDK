439e120ae765b4933ceb912ba05c527cc8954ec9
Levent Tasdemir <61621022+Light4Code@users.noreply.github.com>
2023-04-05 20:20:34 +0200

    Issue/immutable collection (#15)
    * Change from ReadOnlyCollection to ImmutableList
    
    * Change from ReadOnlyCollection to ImmutableList
    
    * Fixed immutable cast

--- src/Common/Ports/NumericCollectionPort.cs
+++ src/Common/Ports/NumericCollectionPort.cs
@@ -1,17 +1,40 @@
-using System.Collections.ObjectModel;
+using System.Collections.Immutable;
 
 namespace AyBorg.SDK.Common.Ports;
 
-public class NumericCollectionPort : ValuePortGeneric<NumericCollectionPort, ReadOnlyCollection<double>>
+public class NumericCollectionPort : ValuePortGeneric<NumericCollectionPort, ImmutableList<double>>
 {
-    public NumericCollectionPort(string name, PortDirection direction, ReadOnlyCollection<double> value) : base(name, direction, value)
+    /// <summary>
+    /// Initializes a new instance of the <see cref="NumericCollectionPort"/> class.
+    /// </summary>
+    /// <param name="name">The name.</param>
+    /// <param name="direction">The port direction.</param>
+    public NumericCollectionPort(string name, PortDirection direction) : base(name, direction)
+    {
+        Value = ImmutableList<double>.Empty;
+    }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="NumericCollectionPort"/> class.
+    /// </summary>
+    /// <param name="name">The name.</param>
+    /// <param name="direction">The port direction.</param>
+    /// <param name="value">The value.</param>
+    public NumericCollectionPort(string name, PortDirection direction, ImmutableList<double> value) : base(name, direction, value)
     {
     }
 
+    /// <summary>
+    /// Initializes a new instance of the <see cref="NumericCollectionPort"/> class.
+    /// </summary>
+    /// <param name="other">The other.</param>
     public NumericCollectionPort(NumericCollectionPort other) : base(other)
     {
-        Value = new ReadOnlyCollection<double>(other.Value);
+        Value = ImmutableList<double>.Empty.AddRange(other.Value);
     }
 
+    /// <summary>
+    /// Gets the brand.
+    /// </summary>
     public override PortBrand Brand => PortBrand.NumericCollection;
 }
