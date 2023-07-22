d3f10677c130e3a61c863dc056bfccaf6078d56b
Levent Tasdemir <light4code@gmail.com>
2023-07-22 19:35:53 +0200

    Documentation: Added device manager summary

--- src/Common/IDeviceManager.cs
+++ src/Common/IDeviceManager.cs
@@ -2,12 +2,31 @@ namespace AyBorg.SDK.Common;
 
 public interface IDeviceManager
 {
+    /// <summary>
+    /// Occurs when a device is changed.
+    /// </summary>
     event EventHandler<ObjectChangedEventArgs> DeviceChanged;
+
+    /// <summary>
+    /// Occurs when a device is added or removed.
+    /// </summary>
     event EventHandler<CollectionChangedEventArgs> DeviceCollectionChanged;
 
+    /// <summary>
+    /// Gets the devices.
+    /// </summary>
     IEnumerable<IDevice> Devices { get; }
 
+    /// <summary>
+    /// Gets the devices.
+    /// </summary>
+    /// <returns>The devices.</returns>
     IEnumerable<T> GetDevices<T>() where T : IDevice;
 
+    /// <summary>
+    /// Gets the device.
+    /// </summary>
+    /// <param name="deviceId">The device identifier.</param>
+    /// <returns>The device.</returns>
     T GetDevice<T>(string deviceId) where T : IDevice;
 }
