a97d16d00fc9bb07656f04be1a7edd4f9eec692e
Levent Tasdemir <light4code@gmail.com>
2023-08-05 17:42:38 +0200

    Added dashboard type

--- src/System/ServiceTypes.cs
+++ src/System/ServiceTypes.cs
@@ -4,6 +4,7 @@ public static class ServiceTypes
 {
     public const string Gateway = "Gateway";
     public const string Web = "Web";
+    public const string Dashboard = "Dashboard";
     public const string Agent = "Agent";
     public const string Analytics = "Analytics";
     public const string Audit = "Audit";
