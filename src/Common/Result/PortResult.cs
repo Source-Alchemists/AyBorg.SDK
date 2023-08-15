cec663edb771a4c88b22fd7e540688e9445e5b42
Levent Tasdemir <light4code@gmail.com>
2023-08-15 20:38:22 +0200

    Feature: Result storage contract

--- src/Common/Result/PortResult.cs
+++ src/Common/Result/PortResult.cs
@@ -6,6 +6,7 @@ public record PortResult
 {
     public string Id { get; } = string.Empty;
     public Port Port { get; } = new Port();
+    public float ScaleFactor { get; init; } = 1f;
 
     public PortResult(string id, Port port)
     {
