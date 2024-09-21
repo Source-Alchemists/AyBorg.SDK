106e47bbc1dee6ef2d6f2c3f3063d93640c66949
Levent Tasdemir <light4code@gmail.com>
2024-09-21 12:55:38 +0200

    Nuget update

--- src/Common/Communication/IMessage.cs
+++ src/Common/Communication/IMessage.cs
@@ -2,5 +2,5 @@ namespace AyBorg.SDK.Common.Communication;
 
 public interface IMessage {
     string ContentType { get; init; }
-    byte[] Payload { get; init; }
+    ArraySegment<byte> Payload { get; init; }
 }
