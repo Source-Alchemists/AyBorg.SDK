6ffb7d42a70fe76a2e8149a3b0c2bdb542f8a9c7
Levent Tasdemir <light4code@gmail.com>
2024-09-22 15:22:25 +0200

    Update Nuget packages and fix vulnerabilities

--- src/Common/Environment.cs
+++ src/Common/Environment.cs
@@ -1,12 +1,14 @@
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
 
 namespace AyBorg.SDK.Common;
 
 public class Environment : IEnvironment
 {
-    public Environment(IConfiguration configuration)
+    public Environment(ILogger<Environment> logger, IConfiguration configuration)
     {
         StorageLocation = configuration.GetValue("Storage:Folder", Path.Combine(System.Environment.GetFolderPath(System.Environment.SpecialFolder.ApplicationData), "AyBorg", "Storage"))!;
+        logger.LogInformation("Environment>Storage>Folder: {StorageLocation}", StorageLocation);
     }
 
     /// <summary>
